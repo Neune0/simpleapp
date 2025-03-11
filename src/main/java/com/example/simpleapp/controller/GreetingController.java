2140ea6450574db4919cd5c4541060124c03d44c
Raimon <60296186+Neune0@users.noreply.github.com>
2025-03-11 09:36:23 +0100

    primo service controller

---
+++ src/main/java/com/example/simpleapp/controller/GreetingController.java
@@ -0,0 +1,23 @@
+package com.example.simpleapp.controller;
+import com.example.simpleapp.service.GreetingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    private final GreetingService greetingService;
+
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/greet")
+    public String greet(){
+        return greetingService.getGreeting();
+    }
+
+    
+}
